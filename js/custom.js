67729d66fa70e8b30aff5efbec5529ebab41e6b9
Vilumn <72590764+Vilumn@users.noreply.github.com>
2021-07-20 23:12:49 +0700

    pala geser

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+let button_matt_change = document.querySelector("#change-matt-head")
+
+
+
+
+button_matt_change.addEventListener("click", () => {
+    let matt_head1 = document.querySelector("#matt-head1")
+    let matt_head2 = document.querySelector("#matt-head2")
+
+    console.log(matt_head2.style.display)
+
+    if (matt_head2.style.display == "none"){
+        matt_head1.style.display = "none"
+        matt_head2.style.display = "inline"
+    }else{
+        matt_head1.style.display = "inline"
+        matt_head2.style.display = "none"
+    }
+
+    
+})
